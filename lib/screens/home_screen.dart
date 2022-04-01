ec81e98d5c6ed7022203f8b113597744875a8505
liyu158163 <991351471@qq.com>
2022-04-01 19:47:37 +0800

    新增home_screen.dart。
    新增home_screen.dart。

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: const DecorationImage(
+                fit: BoxFit.cover, image: AssetImage("assets/welcome.png"))),
+      ),
+    );
+  }
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static final mainColor = Color(0xFF202e59);
+  static final secondarycolor = Color(0xEF6c8ee3);
+  static final smallTextcolor = Color(0xFF2e3253);
+  static final textHolder = Color(0xFFedf0f8);
+  static final textGrey = Color(0xFFafb1b5);
+}

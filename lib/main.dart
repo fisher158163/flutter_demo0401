fc97ff62ad35895bf4c7531ac2739b12c32847b8
liyu158163 <991351471@qq.com>
2022-04-01 23:28:29 +0800

    新增add_task.dart。
    新增add_task.dart。

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo0401/screens/add_task.dart';
 import 'package:flutter_demo0401/screens/home_screen.dart';
 
 void main() {
--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_demo0401/utils/app_colors.dart';
 
 class AddTask extends StatelessWidget {
   const AddTask({Key? key}) : super(key: key);
@@ -7,7 +8,31 @@ class AddTask extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover, image: AssetImage("assets/welcome.png"))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back),
+                  color: AppColors.secondarycolor,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
